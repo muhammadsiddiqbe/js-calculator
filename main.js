fac1349ac240e3d86786d955332d3621235c94fa
MuhammadSiddiq <707myworld@gmail.com>
2020-12-10 19:57:16 +0500

    initial commit and end project :( because I forgot pushing

--- main.js
+++ main.js
@@ -1,8 +1,65 @@
-function calc(){
+function calculator(x, y){
+  this.x = x;
+  this.y = y;
+};
 
-}
 
-Object.prototype.binnima = function(){
-  // console.log('binnima');
-  return 'Vahahaha'
-}
+calculator.prototype.addition = function() {
+  return  this.x + this.y;
+};
+calculator.prototype.difference = function() {
+  return  this.x - this.y;
+};
+calculator.prototype.division = function() {
+  return  this.x / this.y;
+};
+calculator.prototype.product = function() {
+  return  this.x * this.y;
+};
+calculator.prototype.module = function() {
+  return  this.x % this.y;
+};
+calculator.prototype.power = function() {
+  return  this.x ** this.y;
+};
+
+let calculation;
+
+
+calcForm.onsubmit = (evt) => {
+  evt.preventDefault();
+
+  let elinputX = Number(inputX.value);
+  let elSelectOperation = selectOperation.value;
+  let elinputY= Number(inputY.value);
+  let elResult = solution;
+
+  calculation = new calculator(elinputX,elinputY);
+
+  switch (elSelectOperation) {
+    case 'addition':
+      console.log(calculation.addition());
+      elResult.textContent = calculation.addition();
+      break;
+    case 'division':
+      console.log(calculation.division());
+      elResult.textContent = calculation.division();
+      break;
+    case 'product':
+       console.log(calculation.product());
+       elResult.textContent = calculation.product();
+      break;
+    case 'difference':
+      console.log(calculation.difference());
+      break;calculation.difference();
+    case 'module':
+      console.log(calculation.module());
+      elResult.textContent = calculation.module();
+      break;
+    case 'power':
+      console.log(calculation.power());
+      elResult.textContent = calculation.power();
+      break;
+  };
+
+};
